2de533041ebe5a5680c3dbd556627021d7d35288
JP Soliz Molina <jpsoliz@gmail.com>
2025-07-08 00:28:02 -0400

    Add files via upload
    udpates

---
+++ enterprise/SID_Measure/dist/runtime/translations/default.ts
@@ -0,0 +1,9 @@
+const defaultMessages = {
+  _widgetLabel: "SID Measure",
+  distMeasureTool: "Distance",
+  areaMearureTool: "Area",
+  clearMeasure: "Clear",
+  pointMeasureTool: "Point"
+};
+
+export default defaultMessages;
